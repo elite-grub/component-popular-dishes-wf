4d3c84f86d8330fe1c3f2e6db71384f383a5258f
Winnie Fong <winniefong21@gmail.com>
2019-02-24 01:57:23 -0800

    Setup airbnb style eslint

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <h2>Popular Dishes under construction...</h2>, document.getElementById('app')
+  <h2>Popular Dishes under construction...</h2>, document.getElementById('app'),
 );
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,6 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-react', '@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-class-properties'],
           },
         },
       },
