db027143b26b72045547cf7db0f8af9df545e6a8
Winnie Fong <winniefong21@gmail.com>
2019-02-25 16:19:35 -0800

    Update static file path

--- server/index.js
+++ server/index.js
@@ -7,11 +7,13 @@ const { findRestaurant } = require('../database/index.js');
 const app = express();
 const port = 3030;
 
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.get('/:id', (req, res) => {
+app.get('/popular/:id', (req, res) => {
   const { id } = req.params;
   findRestaurant(id, (err, data) => {
+    console.log('SERVER: ', data);
+    console.log('SERVER ID: ', id);
     if (err) {
       res.sendStatus(400);
       return;
