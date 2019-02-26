8081e814fb08f3a311f02b5bd541ebf9c3551e57
Winnie Fong <winniefong21@gmail.com>
2019-02-26 13:40:26 -0800

    Able to render App

---
+++ client/src/components/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+// class App extends React.component {
+//   constructor(props) {
+//     super(props);
+//     this.state = '';
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello World!</h1>
+//       </div>
+//     );
+//   }
+// }
+
+const App = () => (
+  <div>
+    <h1>Hello World!</h1>
+  </div>
+);
+
+export default App;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './components/App.jsx';
+
 ReactDOM.render(
-  <h2>Popular Dishes under construction...</h2>, document.getElementById('app'),
+  <App />, document.getElementById('app'),
 );
