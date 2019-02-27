7554944478802ed9e19bdead62846e9fed2e0199
Winnie Fong <winniefong21@gmail.com>
2019-02-26 22:52:37 -0800

    Write tests for App rendering child components

--- tests/test.js
+++ tests/test.js
@@ -1,17 +1,23 @@
 import { configure, shallow, render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
-import sinon from 'sinon';
 // import PropTypes from 'prop-types';
 import { expect } from 'chai';
 
 import App from '../client/src/components/App.jsx';
+import Header from '../client/src/components/Header.jsx';
+import ImageCarousel from '../client/src/components/ImageCarousel.jsx';
 
 configure({ adapter: new Adapter() });
 
-describe('<App />', () => {
-  it('renders two <App /> components', () => {
+describe('App Testing', () => {
+  it('renders Header component', () => {
     const wrapper = shallow(<App />);
-    expect(wrapper.find(App)).to.have.lengthOf(2);
+    expect(wrapper.find(Header)).to.have.lengthOf(1);
+  });
+
+  it('renders ImageCarousel component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(ImageCarousel)).to.have.lengthOf(1);
   });
 });
