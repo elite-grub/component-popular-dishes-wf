2393885b2673f5d2ad563c6b66b45ccc54fd0718
Winnie Fong <winniefong21@gmail.com>
2019-02-27 12:05:18 -0800

    Refactor tests to not require Chai

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,7 +1,6 @@
 import { configure, shallow, render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
-import { expect } from 'chai';
 
 import Header from '../client/src/components/Header.jsx';
 import LeftNav from '../client/src/components/LeftNav.jsx';
@@ -12,21 +11,21 @@ configure({ adapter: new Adapter() });
 describe('Header Tests', () => {
   it('renders the Popular Dishes title', () => {
     const wrapper = render(<Header className="title" />);
-    expect(wrapper.text()).to.contain('Popular Dishes');
+    expect(wrapper.text()).toContain('Popular Dishes');
   });
 
   it('renders the view full menu link', () => {
     const wrapper = render(<Header className="menu" />);
-    expect(wrapper.html()).to.contain('a');
+    expect(wrapper.html()).toContain('a');
   });
 
   it('renders carousel-nav left component', () => {
     const wrapper = shallow(<Header />);
-    expect(wrapper.find(LeftNav)).to.have.lengthOf(1);
+    expect(wrapper.find(LeftNav)).toHaveLength(1);
   });
 
   it('renders carousel-nav right component', () => {
     const wrapper = shallow(<Header />);
-    expect(wrapper.find(RightNav)).to.have.lengthOf(1);
+    expect(wrapper.find(RightNav)).toHaveLength(1);
   });
 });
--- tests/imageCarousel.test.js
+++ tests/imageCarousel.test.js
@@ -1,7 +1,6 @@
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
-import { expect } from 'chai';
 
 import ImageCarousel from '../client/src/components/ImageCarousel.jsx';
 import DishPhotos from '../client/src/components/DishPhotos.jsx';
@@ -11,6 +10,6 @@ configure({ adapter: new Adapter() });
 describe('Image Carousel Tests', () => {
   it('renders DishPhotos component', () => {
     const wrapper = shallow(<ImageCarousel />);
-    expect(wrapper.find(DishPhotos)).to.have.lengthOf(1);
+    expect(wrapper.find(DishPhotos)).toHaveLength(1);
   });
 });
