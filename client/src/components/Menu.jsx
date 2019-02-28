1f3fb070b36d2e0ca469b3365989322c134a2cf9
Winnie Fong <winniefong21@gmail.com>
2019-02-27 18:03:38 -0800

    Able to get menu url from db

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -6,24 +6,42 @@ import '../../dist/styles.css';
 class Menu extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      menuURL: '',
+    };
 
-    // this.getMenuURL = this.getMenuURL.bind(this);
+    this.getMenuURL = this.getMenuURL.bind(this);
   }
 
-  // componentDidMount() {
-  //   this.getMenuURL();
-  // }
+  componentDidMount() {
+    this.getMenuURL();
+  }
 
-  // getMenuURL() {
-  //   $.get('')
-  // }
+  getMenuURL() {
+    const getData = (callback) => {
+      $.get({
+        url: 'http://localhost:3030/popular/:1',
+        success: data => callback(null, data.menuURL),
+        error: err => callback(err),
+      });
+    };
+
+    getData((err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      this.setState({
+        menuURL: data,
+      });
+    });
+  }
 
   render() {
     return (
       <div className="menu">
         <a className="menu-link"
-          href="https://www.google.com/">View Full Menu</a>
+          href={this.state.menuURL}>View Full Menu</a>
       </div>
     );
   }
