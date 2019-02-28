d7aa1af9c567237a0ff44f3ff11546fff6ca6a17
Winnie Fong <winniefong21@gmail.com>
2019-02-28 00:26:17 -0800

    Set up bootstrap

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,27 +5,27 @@ import ImageCarousel from './ImageCarousel.jsx';
 
 import '../../dist/styles.css';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {};
+//   }
 
-  render() {
-    return (
-      <div className="container">
-        <Header />
-        <ImageCarousel />
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div className="container">
+//         <Header />
+//         <ImageCarousel />
+//       </div>
+//     );
+//   }
+// }
 
-// const App = () => (
-//   <div>
-//     <Header />
-//     <ImageCarousel />
-//   </div>
-// );
+const App = () => (
+  <div className="container">
+    <Header />
+    <ImageCarousel />
+  </div>
+);
 
 export default App;
